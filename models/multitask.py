8f9efbfe25e19f74c93e08b8f57bddfdc72d7a5b
neer-patel-11 <patelneer403@gmail.com>
2026-04-08 23:12:35 +0530

    up

--- models/multitask.py
+++ models/multitask.py
@@ -37,7 +37,7 @@ class MultiTaskPerceptionModel(nn.Module):
         )
         gdown.download(id="19Grc7A4q9J6Dq9dJM9w9VOscjQu_V-o9", output=classifier_path, quiet=False)
         gdown.download(id="1BpOe9YyojShsXoSTBvdBflrubHEF2wQK", output=localizer_path, quiet=False)
-        gdown.download(id="1H59EmgH6IACggQ_jaSY0OAGPwz2Ai7WU", output=unet_path, quiet=False)
+        gdown.download(id="1sw0a8if_hrFzuzRI6v29_LNz22UMDUAg", output=unet_path, quiet=False)
 
         self.classifier = VGG11Classifier(num_classes=num_breeds)
         self.localizer = VGG11Localizer()
